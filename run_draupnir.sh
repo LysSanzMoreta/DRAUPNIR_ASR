2ef0a15d564bff3bdaf62866e3d9b2fe9510f9a3
Lys Sanz Moreta <moreta@sund.ku.dk>
2026-01-21 13:27:12 +0100

    updated bash commands to run

--- run_draupnir.sh
+++ run_draupnir.sh
@@ -2,36 +2,43 @@
 
 
 
-python Draupnir_example.py -name simulations_1GMM -n 10000 -n-samples 200 -use-blosum True -use-cuda True -draupnir-version 1 -bsize 100 -build False #we only have 4000 epochs
-python Draupnir_example.py -name simulations_1GMM -n 10000 -n-samples 200 -use-blosum False -use-cuda True -draupnir-version 1 -bsize 100
-python Draupnir_example.py -name simulations_1GMM -n 10000 -n-samples 200 -use-blosum False -use-cuda True -draupnir-version 3a -bsize 100
-python Draupnir_example.py -name simulations_1GMM -n 10000 -n-samples 200 -use-blosum False -use-cuda True -draupnir-version 3b -bsize 100 #already done
+#python Draupnir_example.py -name simulations_1GMM -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 100 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_1GMM -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 10 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_1GMM -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 100 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_1GMM -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 100 -use-cuda True -generate-samples False
 
-python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 50
-python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 50
+#python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 50 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 50 -use-cuda True -generate-samples False
+python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 50 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_3 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 50 -use-cuda True -generate-samples False
+#
+#python Draupnir_example.py -name simulations_src_sh3_2 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 100 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_2 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 100 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_2 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 100 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_2 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 100 -use-cuda True -generate-samples False
+#
+#python Draupnir_example.py -name simulations_src_sh3_1 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_src_sh3_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 1 -use-cuda True -generate-samples False
+#
+##150 leaves -generate-samples False
+#python Draupnir_example.py -name simulations_sirtuins_1 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_sirtuins_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_sirtuins_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_sirtuins_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 1 -use-cuda True -generate-samples False
 
+#300 leaves -generate-samples False
+python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 50 -use-cuda True -generate-samples False
+python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 50 -use-cuda True -generate-samples False
+python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 50 -use-cuda True -generate-samples False
+python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 50 -use-cuda True -generate-samples False
 
-python Draupnir_example.py -name simulations_src_sh3_2 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_src_sh3_2 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_src_sh3_2 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_src_sh3_2 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 50
-
-
-python Draupnir_example.py -name simulations_src_sh3_1 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 1
-python Draupnir_example.py -name simulations_src_sh3_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 1
-python Draupnir_example.py -name simulations_src_sh3_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 1
-python Draupnir_example.py -name simulations_src_sh3_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 1
-
-#150 leaves
-python Draupnir_example.py -name simulations_sirtuins_1 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_sirtuins_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_sirtuins_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 50
-python Draupnir_example.py -name simulations_sirtuins_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 50
-
-#300 leaves
-python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 50
-python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 50
-python Draupnir_example.py -name simulations_PIGBOS_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 50
+#python Draupnir_example.py -name simulations_blactamase_1 -n 10000 -n-samples 200 -use-blosum True -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_blactamase_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 1 -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_blactamase_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3a -bsize 1 -use-cuda True -generate-samples False
+#python Draupnir_example.py -name simulations_blactamase_1 -n 10000 -n-samples 200 -use-blosum False -draupnir-version 3b -bsize 1 -use-cuda True -generate-samples False
