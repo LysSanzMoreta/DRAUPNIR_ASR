made fix to sampling covariance, test passing again

draupnir/src/draupnir/ablation_studies/Draupnir_versions.py
@@ -145,7 +145,6 @@ def lowass_calculation(tree_results_dict,metrics_dict,metric_array,metric_name,l
 
     return lowass_dict
 
-
 def lowass_scores(storage_folder:str,dataset_name:str, mode_name:str, metrics_dict:dict,model_output:dict,lowass_dict:Union[dict | None]):
     """Calculation of the LOWESS (Locally Weighted Scatterplot Smoothing) between the """
 
@@ -245,78 +244,50 @@ def metrics(predictions_dict:dict,mode_name:str="") -> dict:
     return metrics_dict
 
 
-#todo: include batched vs no batched
-#classic_blosum uses the blosum weighted
-folders_dict = {
-    "simulations_src_sh3_1": {
-        "draupnir_classic_blosum": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_1_2026_01_20_21h08min29s151422ms_10000epochs_variational",
-        "draupnir_classic_no_blosum": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_1_2026_01_20_21h31min54s951820ms_10000epochs_variational",
-        "draupnir_z_esm": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_1_2026_01_19_20h23min58s667738ms_10000epochs_variational",
-        "draupnir_hidden_esm": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_1_2026_01_19_20h40min49s587822ms_10000epochs_variational",
+folders_dict = {  "simulations_src_sh3_3": {
+        "draupnir_classic_blosum": "/home/lys/Dropbox/PhD/DRAUPNIR_ASR/PLOTS_Draupnir_simulations_src_sh3_3_2026_04_13_17h36min09s766115ms_8000epochs_variational",
+        "draupnir_classic_no_blosum": "/home/lys/Dropbox/PhD/DRAUPNIR_ASR/PLOTS_Draupnir_simulations_src_sh3_3_2026_04_13_16h04min53s711255ms_8000epochs_variational",
+        # "draupnir_z_esm": "",
+        # "draupnir_hidden_esm": "",
+        "draupnir_whitening_blosum": "/home/lys/Dropbox/PhD/DRAUPNIR_ASR/PLOTS_Draupnir_simulations_src_sh3_3_2026_04_14_05h11min51s663287ms_8000epochs_variational",
+        "draupnir_whitening_no_blosum": "/home/lys/Dropbox/PhD/DRAUPNIR_ASR/PLOTS_Draupnir_simulations_src_sh3_3_2026_04_14_03h50min30s581252ms_8000epochs_variational",
+        "draupnir_no_ou_params_blosum": "/home/lys/Dropbox/PhD/DRAUPNIR_ASR/PLOTS_Draupnir_simulations_src_sh3_3_2026_04_13_23h24min52s453150ms_8000epochs_variational",
+        "draupnir_no_ou_params_no_blosum": "/home/lys/Dropbox/PhD/DRAUPNIR_ASR/PLOTS_Draupnir_simulations_src_sh3_3_2026_04_13_19h11min56s768538ms_8000epochs_variational",
     },
-    "simulations_src_sh3_2": {
-        "draupnir_classic_blosum": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_2_2026_01_16_19h25min29s965361ms_10000epochs_variational",
-        "draupnir_classic_no_blosum": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_2_2026_01_16_23h26min19s155055ms_10000epochs_variational",
-        "draupnir_z_esm": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_2_2026_01_19_12h14min37s844603ms_10000epochs_variational",
-        "draupnir_hidden_esm": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_2_2026_01_19_14h44min01s395934ms_10000epochs_variational",
-    },
-    "simulations_src_sh3_3": {
-        "draupnir_classic_blosum": "",
-        "draupnir_classic_no_blosum": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_3_2026_01_16_16h29min59s341296ms_10000epochs_variational",
-        "draupnir_z_esm": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_3_2026_01_16_17h35min55s614522ms_10000epochs_variational",
-        "draupnir_hidden_esm": "draupnir_models/PLOTS_Draupnir_simulations_src_sh3_3_2026_01_16_18h19min00s229714ms_10000epochs_variational",
-    },
-    "simulations_blactamase_1": {
-        "draupnir_classic_blosum": "draupnir_models/PLOTS_Draupnir_simulations_blactamase_1_2026_01_20_21h53min12s628284ms_10000epochs_variational",
-        "draupnir_classic_no_blosum": "draupnir_models/PLOTS_Draupnir_simulations_blactamase_1_2026_01_20_23h10min50s249395ms_10000epochs_variational",
-        "draupnir_z_esm": "draupnir_models/PLOTS_Draupnir_simulations_blactamase_1_2026_01_21_00h23min24s640140ms_10000epochs_variational",
-        "draupnir_hidden_esm": "draupnir_models/PLOTS_Draupnir_simulations_blactamase_1_2026_01_21_01h08min11s963248ms_10000epochs_variational",
-    },
-    "simulations_calcitonin_1": {
-        "draupnir_classic_blosum":"draupnir_models/PLOTS_Draupnir_simulations_calcitonin_1_2026_01_29_18h43min49s404503ms_10000epochs_variational",
-        "draupnir_classic_no_blosum":"draupnir_models/PLOTS_Draupnir_simulations_calcitonin_1_2026_01_29_19h01min07s450467ms_10000epochs_variational",
-        "draupnir_z_esm":"draupnir_models/PLOTS_Draupnir_simulations_calcitonin_1_2026_01_29_19h17min09s563094ms_10000epochs_variational",
-        "draupnir_hidden_esm":"draupnir_models/PLOTS_Draupnir_simulations_calcitonin_1_2026_01_29_19h29min23s604870ms_10000epochs_variational",
-                                 },
-    "simulations_sirtuins_1": {
-        "draupnir_classic_blosum": "draupnir_models/PLOTS_Draupnir_simulations_sirtuins_1_2026_01_19_21h01min46s029776ms_10000epochs_variational",
-        "draupnir_classic_no_blosum": "draupnir_models/PLOTS_Draupnir_simulations_sirtuins_1_2026_01_19_23h39min47s797048ms_10000epochs_variational",
-        "draupnir_z_esm": "draupnir_models/PLOTS_Draupnir_simulations_sirtuins_1_2026_01_20_01h59min03s763175ms_10000epochs_variational",
-        "draupnir_hidden_esm": "draupnir_models/PLOTS_Draupnir_simulations_sirtuins_1_2026_01_20_03h18min33s750688ms_10000epochs_variational",
-    },
-    "simulations_1GMM": {
-        "draupnir_classic_blosum": "draupnir_models/PLOTS_Draupnir_simulations_1GMM_2026_01_15_18h11min01s898288ms_10000epochs_variational",
-        "draupnir_classic_no_blosum": "draupnir_models/PLOTS_Draupnir_simulations_1GMM_2026_01_16_00h59min19s996824ms_10000epochs_variational",
-        "draupnir_z_esm": "draupnir_models/PLOTS_Draupnir_simulations_1GMM_2026_01_16_06h38min12s618111ms_10000epochs_variational",
-        "draupnir_hidden_esm": "draupnir_models/PLOTS_Draupnir_simulations_1GMM_2026_01_15_18h11min01s898288ms_10000epochs_variational",
-    },
+    # "simulations_1GMM": {
+    #     "draupnir_classic_blosum": "",
+    #     "draupnir_classic_no_blosum": "",
+    #     # "draupnir_z_esm": "",
+    #     # "draupnir_hidden_esm": "",
+    #     "draupnir_whitening": "",
+    #     "draupnir_no_ou_params": ""
+    # },
 
 }
 
-
-folders_dict = {  "simulations_src_sh3_3": {
+folders_dict_batching = {  "simulations_src_sh3_3": {
         "draupnir_classic_blosum": "",
-        "draupnir_classic_no_blosum": "",
-        "draupnir_z_esm": "",
-        "draupnir_hidden_esm": "",
-        "draupnir_whitening": "",
-        "draupnir_no_ou_params": "",
+        "draupnir_classic_no_blosum": "/home/lys/Dropbox/PhD/DRAUPNIR_ASR/PLOTS_Draupnir_simulations_src_sh3_3_2026_04_13_16h41min04s629902ms_8000epochs_variational_batched_no_blosum",
+        # "draupnir_z_esm": "",
+        # "draupnir_hidden_esm": "",
+        "draupnir_whitening_blosum": "",
+        "draupnir_whitening_no_blosum": "",
+        "draupnir_no_ou_params_blosum": "",
+        "draupnir_no_ou_params_no_blosum": "",
     },
     "simulations_1GMM": {
         "draupnir_classic_blosum": "",
         "draupnir_classic_no_blosum": "",
-        "draupnir_z_esm": "",
-        "draupnir_hidden_esm": "",
+        # "draupnir_z_esm": "",
+        # "draupnir_hidden_esm": "",
+        "draupnir_whitening": "",
         "draupnir_no_ou_params": ""
     },
 
-
-
-
 }
 
 
-def analyze(results_dict=None):
+def analyze(folders_dict,results_dict=None):
 
 
     if not results_dict is not None:
@@ -354,7 +325,7 @@ def analyze(results_dict=None):
                 results_dict[dataset_name][f"{mode}_test"] = metrics_dict
                 results_dict[dataset_name][f"{mode}_consensus"] = metrics_dict["consensus_metrics"]
                 del results_dict[dataset_name][f"{mode}_test"]["consensus_metrics"] #otherwise it is saved twice
-                torch.save(results_dict,"draupnir_models/metrics/results_dict2.torch")
+                torch.save(results_dict,"draupnir_models/metrics/results_dict.torch")
             else:
                 print(f"{mode} found")
 
@@ -364,16 +335,17 @@ def analyze(results_dict=None):
     return results_dict
 
 
-#results_dict = torch.load("draupnir_models/metrics/results_dict.torch",weights_only=False)
-analyze(None)
-#analyze(results_dict)
-
-exit()
-
 
 def build_metrics_table(results_dict,name):
     table_keys = ["average_pid","average_pid_std","average_cosine_similarity","correlations_leaves_samples_mi"]
-    skip = ["draupnir_classic_no_blosum_consensus","draupnir_z_esm_consensus","draupnir_hidden_esm_consensus"]
+    skip = ["draupnir_classic_no_blosum_consensus",
+            "draupnir_z_esm_consensus",
+            "draupnir_hidden_esm_consensus",
+            "draupnir_whitening_blosum_consensus",
+            "draupnir_whitening_no_blosum_consensus",
+            "draupnir_no_ou_params_no_blosum_consensus",
+            "draupnir_no_ou_params_blosum_consensus",
+            ]
     results_dict_reoriented = defaultdict(dict)
     for dataset in results_dict.keys():
         for model in results_dict[dataset].keys():
@@ -401,6 +373,15 @@ def build_metrics_table(results_dict,name):
         "draupnir_z_esm_test": matplotlib.colors.to_hex("violet"),
         "draupnir_hidden_esm_train": matplotlib.colors.to_hex("dodgerblue"),
         "draupnir_hidden_esm_test": matplotlib.colors.to_hex("royalblue"),
+        "draupnir_whitening_blosum_train": matplotlib.colors.to_hex("dodgerblue"),
+        "draupnir_whitening_blosum_test": matplotlib.colors.to_hex("royalblue"),
+        "draupnir_whitening_no_blosum_train": matplotlib.colors.to_hex("darkgoldenrod"),
+        "draupnir_whitening_no_blosum_test": matplotlib.colors.to_hex("gold"),
+        "draupnir_no_ou_params_blosum_train": matplotlib.colors.to_hex("mediumorchid"),
+        "draupnir_no_ou_params_blosum_test": matplotlib.colors.to_hex("violet"),
+        "draupnir_no_ou_params_no_blosum_train": matplotlib.colors.to_hex("magenta"),
+        "draupnir_no_ou_params_no_blosum_test": matplotlib.colors.to_hex("pink"),
+
 
               }
 
@@ -442,14 +423,26 @@ def compute_lowass_curves(results_dict):
     #             "no": [matplotlib.colors.to_hex("orangered"),matplotlib.colors.to_hex("darkorange")],
     #                }
 
-    colors_dict2 = {
-                "hidden_esm": ["dodgerblue","royalblue"],
-                "z_esm": ["mediumorchid","violet"],
-                "classic_blosum_consensus": ["turquoise", "turquoise"], #keep in this order
-                "classic_blosum": ["forestgreen","greenyellow"],
-                "no_blosum": ["orangered","darkorange"],
+    # colors_dict2 = {
+    #             "hidden_esm": ["dodgerblue","royalblue"],
+    #             "z_esm": ["mediumorchid","violet"],
+    #             "classic_blosum_consensus": ["turquoise", "turquoise"], #keep in this order
+    #             "classic_blosum": ["forestgreen","greenyellow"],
+    #             "no_blosum": ["orangered","darkorange"],
+    #
+    #                }
 
+
+    colors_dict2 = {
+                "classic_blosum": ["forestgreen", "greenyellow"],
+                "classic_no_blosum": ["orangered", "darkorange"],
+                "whitening_blosum": ["dodgerblue","royalblue"],
+                "whitening_no_blosum": ["darkgoldenrod","gold"],
+                "no_ou_params_blosum": ["mediumorchid","violet"],
+                "no_ou_params_no_blosum": ["magenta","pink"],
+                "classic_blosum_consensus": ["turquoise", "turquoise"], #keep in this order
                    }
+
     #skip = ["draupnir_classic_no_blosum_consensus", "draupnir_classic_z_esm_consensus","draupnir_classic_hidden_esm_consensus"]
 
     skip=[]
@@ -494,6 +487,12 @@ def compute_lowass_curves(results_dict):
 
 
 
+
+
+
+#results_dict = torch.load("draupnir_models/metrics/results_dict.torch",weights_only=False)
+#analyze(folders_dict,None)
+#analyze(folders_dict,results_dict)
 
 results_dict = torch.load("draupnir_models/metrics/results_dict.torch",weights_only=False)
 build_metrics_table(results_dict,"results_dict_metrics")

draupnir/src/draupnir/models.py
@@ -112,9 +112,10 @@ class DRAUPNIRModelClass(nn.Module):
             "5": self.prediction_batched_preprocessing5,
         }
 
-        if self.args.likelihood_type == "potts":
+        if hasattr(self.args, 'likelihood_type'):
+            if self.args.likelihood_type == "potts":
 
-            pass
+                pass
 
 
         if self.args.use_cuda:
@@ -473,7 +474,7 @@ class DRAUPNIRModelClass(nn.Module):
 
     #########################################
 
-    def prediction_preprocessing(self, map_estimates, patristic_matrix_full, patristic_matrix_test, batch_idx,use_test, use_test2):
+    def prediction_preprocessing(self, map_estimates, patristic_matrix_full, patristic_matrix_eval, batch_idx,use_test, use_test2):
         if use_test2: #MAP estimate
             assert patristic_matrix_full[1:,1:].shape == (self.n_all,self.n_all)
             out_prediction_dict = self.conditional_samplingMAP(map_estimates,patristic_matrix_full)
@@ -498,7 +499,7 @@ class DRAUPNIRModelClass(nn.Module):
 
         return {"latent_space": latent_space, "n_nodes": n_nodes, "covariance": covariance}
 
-    def prediction_preprocessing0(self, map_estimates, patristic_matrix_full, patristic_matrix_test, batch_idx, use_test, use_test2):
+    def prediction_preprocessing0(self, map_estimates, patristic_matrix_full, patristic_matrix_eval, batch_idx, use_test, use_test2):
 
         self.leaves_nodes = map_estimates[
             "train_leaves_nodes"] if "train_leaves_nodes" in map_estimates.keys() else self.leaves_nodes
@@ -536,7 +537,7 @@ class DRAUPNIRModelClass(nn.Module):
 
         return {"latent_space": latent_space, "n_nodes": n_nodes, "covariance": covariance}
 
-    def prediction_preprocessing5(self, map_estimates, patristic_matrix_full, patristic_matrix_test, batch_idx,use_test, use_test2):
+    def prediction_preprocessing5(self, map_estimates, patristic_matrix_full, patristic_matrix_eval, batch_idx,use_test, use_test2):
 
         self.leaves_nodes = map_estimates["train_leaves_nodes"] if "train_leaves_nodes" in map_estimates.keys() else self.leaves_nodes
         # if use_test2:  # MAP estimate #todo: make?
@@ -575,15 +576,15 @@ class DRAUPNIRModelClass(nn.Module):
 
         return {"latent_space": latent_space, "n_nodes": n_nodes, "covariance": covariance}
 
-    def prediction_batched_preprocessing(self,map_estimates,patristic_matrix_full,patristic_matrix_test,batch_idx,use_test,use_test2):
+    def prediction_batched_preprocessing(self,map_estimates,patristic_matrix_full,patristic_matrix_eval,batch_idx,use_test,use_test2):
         """Correction of a few parameters to be able to carry on with the batched sampling"""
         if use_test or use_test2:# internal nodes. Only Marginal posterior available when batching
             assert patristic_matrix_full[1:,1:].shape == (self.n_all,self.n_all)
             #Highlight: Slice out the train sequences and only a batch from the test sequences
             if batch_idx[1] is None:
-                self.internal_nodes_batch = patristic_matrix_test[int(batch_idx[0]) + 1:, 0]
+                self.internal_nodes_batch = patristic_matrix_eval[int(batch_idx[0]) + 1:, 0]
             else:
-                self.internal_nodes_batch = patristic_matrix_test[int(batch_idx[0])+1:int(batch_idx[1])+1,0]
+                self.internal_nodes_batch = patristic_matrix_eval[int(batch_idx[0])+1:int(batch_idx[1])+1,0]
             self.n_internal_batch = len(self.internal_nodes_batch)
             self.leaves_nodes = map_estimates["train_leaves_nodes"] if "train_leaves_nodes" in map_estimates.keys() else self.leaves_nodes
             self.n_leaves = len(self.leaves_nodes)
@@ -611,23 +612,18 @@ class DRAUPNIRModelClass(nn.Module):
 
         else: #training/leaves
 
-            n_nodes = self.n_leaves_batch #here n_leaves has been overloaded by the batch size
-            latent_space = map_estimates["latent_z"].T #the map estimates have been pre-concatenated, that is why we have to index them out
+            n_nodes =  self.n_leaves_batch #here n_leaves has been overloaded by the batch size
+            latent_space = map_estimates["latent_z"].T # computed for all the dataset
             latent_space = latent_space[int(batch_idx[0]):int(batch_idx[1])] if batch_idx is not None else latent_space
 
-            # if batch_idx is not None:  # if it is None then the shape should be correct already (for the test_batched_train_batched approach)
-            #     if self.covariance.ndim == 2:
-            #         covariance = self.covariance[batch_idx[0]:batch_idx[1]] if batch_idx[1] is not None else self.covariance[batch_idx[0]:] # this should be in the same order as the predicted dataset (we override the self.covariance when we predict)
-            #     else:
-            #         print("BEFORE !prediction batching preprocessing ", self.covariance.shape)
-            #
-            #         print(batch_idx[0],batch_idx[1])
-            #
-            #         covariance = self.covariance[:,batch_idx[0]:batch_idx[1]] if batch_idx[1] is not None else self.covariance[:,batch_idx[0]:batch_idx[1]]
-            #
-            #         print("AFTER !prediction batching preprocessing ", covariance.shape)
-            # else:
-            covariance = self.covariance #i think here we are returning the covariance from the last batch over and over -> should be overwritten
+            leaves_nodes = map_estimates["node_names"][int(batch_idx[0]):int(batch_idx[1])] if batch_idx is not None else map_estimates["node_names"]
+            #we do not concatenate the covariance matrix, so it is best to recompute it
+            idx_train = (patristic_matrix_full[:,0][...,None] == leaves_nodes).any(-1)
+            idx_train[0] = True
+            patristic_matrix_train = patristic_matrix_full[idx_train]
+            patristic_matrix_train = patristic_matrix_train[:,idx_train]
+            covariance = itemgetter("covariance")(self.gp_prior_batched(patristic_matrix_train))
+            #covariance = self.covariance[:,int(batch_idx[0]):int(batch_idx[1]),int(batch_idx[0]):int(batch_idx[1])] if batch_idx is not None else self.covariance #i think here we are returning the covariance from the last batch over and over -> should be overwritten
 
             assert latent_space.shape == (n_nodes, self.z_dim)
 

draupnir/src/draupnir/train.py
@@ -92,7 +92,9 @@ def fill_estimates(guide_map_estimates,map_estimates,batching=True): #todo: exam
                 map_estimates[key] = val
             else:
                 map_estimates[key] = torch.concat([map_estimates[key], guide_map_estimates[key]], dim=0)
-        elif key in ["embeddings","batch_nodes"]:
+        elif key in ["embeddings","batch_nodes","node_names"]:
+            map_estimates[key] = val
+        else: #todo: danger zone, everything goes
             map_estimates[key] = val
     return  guide_map_estimates, map_estimates
 